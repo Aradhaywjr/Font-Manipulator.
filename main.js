67b1e373a030f0d9e835b0e3b0a77d0b0b1a35e2
Aradhay <96373666+Aradhaywjr@users.noreply.github.com>
2023-05-27 12:04:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup()
+{
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 80);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#fcec03');
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    } 
+}
